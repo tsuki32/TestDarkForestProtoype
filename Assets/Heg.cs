73c280f10b11579236581e23d877c7c967f0b1ff
Lukas <Tsuki32@hotmail.com>
2020-02-28 14:20:20 +0100

    Change test
    Print heh

---
+++ Assets/Heg.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hEH");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
